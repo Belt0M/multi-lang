361b5f96bfb7a3399855bf768abd1c8a950e8611
Belt0M <belongtomars@gmail.com>
2023-09-26 01:54:36 +0200

    New Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,105 @@
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import Select from 'react-select'
 
+interface IColor {
+	value: string
+	label: string
+}
+
+interface ILanguage {
+	name: string
+	value: string
+}
+
 function App() {
-	const colorsOption = [
+	const { t, i18n } = useTranslation()
+
+	const colorsOption: IColor[] = [
 		{
 			value: 'blue',
-			label: 'Blue',
+			label: t('blue'),
 		},
 		{
 			value: 'red',
-			label: 'Red',
+			label: t('red'),
 		},
 		{
 			value: 'white',
-			label: 'White',
+			label: t('white'),
 		},
 		{
 			value: 'black',
-			label: 'Black',
+			label: t('black'),
 		},
 		{
 			value: 'yellow',
-			label: 'Yellow',
+			label: t('yellow'),
 		},
 		{
 			value: 'pink',
-			label: 'Pink',
+			label: t('pink'),
 		},
 		{
 			value: 'brown',
-			label: 'Brown',
+			label: t('brown'),
 		},
 	]
 
+	const lngs: ILanguage[] = [
+		{ name: 'en', value: 'English' },
+		{ name: 'pl', value: 'Polish' },
+	]
+
+	const [isDropped, setIsDropped] = useState<boolean>(false)
+	const [language, setLanguage] = useState<ILanguage>(lngs[0])
+
+	useEffect(() => {
+		i18n.changeLanguage(language.name)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [language])
+
 	return (
-		<Select
-			options={colorsOption}
-			isMulti
-			defaultValue={[colorsOption[0]]}
-			closeMenuOnSelect={false}
-			styles={{
-				container: base => ({
-					...base,
-					width: '20em',
-				}),
-			}}
-		/>
+		<div>
+			<Select
+				options={colorsOption}
+				isMulti
+				closeMenuOnSelect={false}
+				styles={{
+					container: base => ({
+						...base,
+						width: '20em',
+					}),
+				}}
+			/>
+			<div>
+				<button onClick={() => setIsDropped(!isDropped)}>
+					{language.value}
+				</button>
+				{isDropped && (
+					<ul style={{ position: 'fixed', padding: '0rem', margin: 0 }}>
+						{lngs.map(el => (
+							<li
+								key={el.name}
+								style={{
+									listStyle: 'none',
+									padding: '.5rem',
+									background: '#00000050',
+									cursor: 'pointer',
+									margin: 2,
+								}}
+								onClick={() => {
+									setLanguage(el)
+									setIsDropped(false)
+								}}
+							>
+								{el.value}
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+		</div>
 	)
 }
 
---
+++ src/i18n.ts
@@ -0,0 +1,41 @@
+import i18next from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+import { initReactI18next } from 'react-i18next'
+
+i18next
+	.use(initReactI18next)
+	.use(Backend)
+	.use(LanguageDetector)
+	.init({
+		debug: true,
+		fallbackLng: 'en',
+		resources: {
+			en: {
+				translation: {
+					blue: 'Blue',
+					red: 'Red',
+					white: 'White',
+					black: 'Black',
+					yellow: 'Yellow',
+					pink: 'Pink',
+					brown: 'Brown',
+					english: 'English',
+					polish: 'Polish',
+				},
+			},
+			pl: {
+				translation: {
+					blue: 'Niebieski',
+					red: 'Czerwony',
+					white: 'Biały',
+					black: 'Czarny',
+					yellow: 'Żółty',
+					pink: 'Różowy',
+					brown: 'Brązowy',
+					english: 'Angielski',
+					polish: 'Polski',
+				},
+			},
+		},
+	})
