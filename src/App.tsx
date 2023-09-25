ad6e03a82f5f6b5705c1a272d709939146dbcb3f
Belt0M <belongtomars@gmail.com>
2023-09-25 18:30:02 +0200

    Added Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import Select from 'react-select'
+
 function App() {
-	return <div></div>
+	const colorsOption = [
+		{
+			value: 'blue',
+			label: 'Blue',
+		},
+		{
+			value: 'red',
+			label: 'Red',
+		},
+		{
+			value: 'white',
+			label: 'White',
+		},
+		{
+			value: 'black',
+			label: 'Black',
+		},
+		{
+			value: 'yellow',
+			label: 'Yellow',
+		},
+		{
+			value: 'pink',
+			label: 'Pink',
+		},
+		{
+			value: 'brown',
+			label: 'Brown',
+		},
+	]
+
+	return (
+		<Select
+			options={colorsOption}
+			isMulti
+			defaultValue={[colorsOption[0]]}
+			closeMenuOnSelect={false}
+			styles={{
+				container: base => ({
+					...base,
+					width: '20em',
+				}),
+			}}
+		/>
+	)
 }
 
 export default App
